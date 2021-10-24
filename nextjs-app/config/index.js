6e52f2ff689e850df8062ee3dd6fdbf72d06bba4
gnuion <gnu.ion@gmail.com>
2021-10-24 15:42:45 +0200

    changed domain address again

--- nextjs-app/config/index.js
+++ nextjs-app/config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://ergofund.vercel.app";
+  : "https://ergofund-frontend-8l933qh67-gnuion.vercel.app/";
