970a5dddc3eb7f485c16031b6039dfa9807ff7df
Rafael Sorto <rafael@analogrepublic.com>
2021-12-19 15:27:14 -0600

    Installed initial tooling

---
+++ pages/modules.tsx
@@ -0,0 +1,11 @@
+import {Button} from '@chakra-ui/react'
+
+const modules = () => {
+  return (
+    <div>
+      <Button>Chakra UI Button test</Button>
+    </div>
+  )
+}
+
+export default modules
